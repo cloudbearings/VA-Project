19057ee0e25d680502c152597969ac8c83916a3a
Arthur Arioli Bergamaschi <arthurbergmz@gmail.com>
2015-07-28 05:54:47 -0300

    Update Config.java

--- src/com/arthurbergmz/assistant/Config.java
+++ src/com/arthurbergmz/assistant/Config.java
@@ -5,9 +5,9 @@ import com.arthurbergmz.assistant.language.Language;
 public class Config {
 	
 	public static Language LANGUAGE = Language.PORTUGUESE_BRAZIL;
-	public static final String CHROMIUM_API_KEY = "AIzaSyDD3Vr8kh4dZFgualIn1BDyyIVkFyMKkHM";
-	public static final String AMAZON_ACCESS_KEY = "GDNAJFHNDK3AQSWYDSTA";
-	public static final String AMAZON_SECRET_KEY = "dVKslrKgETe2rLKtEvo4316KVhfxj4x7FTQraUpZ";
+	public static final String CHROMIUM_API_KEY = "";
+	public static final String AMAZON_ACCESS_KEY = "";
+	public static final String AMAZON_SECRET_KEY = "";
 	public static final String AMAZON_IVONA_ENDPOINT = "https://tts.eu-west-1.ivonacloud.com";
 	
 }
